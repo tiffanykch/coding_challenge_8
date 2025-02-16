e0127a7ec2e0eba061f2bd348c23c556be122eff
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-16 00:49:27 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -10,3 +10,16 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 calculateSalary(5000, 500, 0.1);
 
 calculateSalary(7000, 1000, 0.15);
+
+// TASK 2: FUNCTION EXPRESSION - PRODUCT PRICE AFTER DISCOUNT
+
+// Create function expression to determine final price of product
+const calculateDiscount = function(price, discountRate) {
+   let finalPrice = price - (price * discountRate);
+   console.log(`Final Price: $${finalPrice.toFixed(2)}`) 
+};
+
+// Test Data
+calculateDiscount(100, 0.2);
+
+calculateDiscount(250, 0.15);
